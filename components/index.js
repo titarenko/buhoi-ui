b29a81626a774b7cb97d73ee5bd7a6681a68ec06
Constantin Titarenko <constantin.titarenko@gmail.com>
2017-03-03 14:43:36 +0200

    add menu, add generic styles

--- components/index.js
+++ components/index.js
@@ -1,4 +1,8 @@
+require('./generic.scss')
+
 module.exports = {
 	List: require('./list'),
 	Multiselect: require('./multiselect'),
+	Menu: require('./menu'),
+	Same: require('./same'),
 }
---
+++ components/menu/component.jsx
@@ -0,0 +1,19 @@
+const { navigateTo } = require('buhoi-client')
+const Same = require('../same')
+
+require('./style.scss')
+
+module.exports = Menu
+
+function Menu ({ route, items, dispatch }) {
+	if (!route) {
+		return <Same />
+	}
+	return <div className="menu">
+		{items.map(it => <a href={it.url} onClick={e => handleClick(e, it.url)} className={route.url.includes(it.url) ? 'active' : null}>{it.title}</a>)}
+	</div>
+	function handleClick (e, url) {
+		e.preventDefault()
+		dispatch(navigateTo(url))
+	}
+}
