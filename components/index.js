167dcad577bd516e70429df724f2e0a39134e591
Erushenko <Erushenko@gmail.com>
2017-03-22 17:00:20 +0200

    add calendar and date-range-filter #6

---
+++ components/index.js
@@ -0,0 +1,12 @@
+require('./generic.scss')
+
+module.exports = {
+	List: require('./list'),
+	Edit: require('./edit'),
+	Multiselect: require('./multiselect'),
+	Menu: require('./menu'),
+	Same: require('./same'),
+	TextInput: require('./text-input'),
+	Calendar: require('./calendar'),
+	DateRangeFilter: require('./date-range-filter'),
+}
