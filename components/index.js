01e4d2fcc23494a18613e23c6d0a1ef621b52d5c
maksem <maksemvovk@gmail.com>
2017-03-23 01:38:46 +0200

    add selectfilter to export #7

---
+++ components/index.js
@@ -0,0 +1,11 @@
+require('./generic.scss')
+
+module.exports = {
+	List: require('./list'),
+	Edit: require('./edit'),
+	Multiselect: require('./multiselect'),
+	Menu: require('./menu'),
+	Same: require('./same'),
+	TextInput: require('./text-input'),
+	Selectfilter: require('./selectfilter'),
+}
