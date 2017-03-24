5bb533cc4ccb06c485c97f6d00141d90dd477ceb
Erushenko <Erushenko@gmail.com>
2017-03-24 19:22:25 +0200

    add reducer's, dispatch's scope into demo page

--- components/index.jsx
+++ components/index.jsx
@@ -47,35 +47,65 @@ if (process.env.NODE_ENV == 'development') {
 
 	const reducers = combineReducers({
 		textInput: TextInput.reducer,
-		dateRangeInput: DateRangeInput.reducer,
+		dateRangeInput: createScopedReducer('dateRangeInput', DateRangeInput.reducer),
+		dateRangeInput2: createScopedReducer('dateRangeInput2', DateRangeInput.reducer),
 	})
 
 	const store = createStore(reducers, applyMiddleware(logger()))
+	const dateRangeInputDispatch = createScopedDispatch('dateRangeInput', store.dispatch)
+	const dateRangeInputDispatch2 = createScopedDispatch('dateRangeInput2', store.dispatch)
 
 	store.subscribe(() => setTimeout(render,0))
-
-	function render () { // eslint-disable-line no-inner-declarations
-		const props = { ...store.getState(), dispatch: store.dispatch }
-		const dom = AllComponents(props)
-		const node = document.getElementById('root')
-		Inferno.render(dom, node)
-	}
-
 	store.dispatch(TextInput.actions.setValue('hi'))
 
 	if (module.hot) {
 		module.hot.accept(() => store.dispatch({ type: 'HOT_RELOAD' }))
 	}
 
-	function AllComponents ({ textInput, dateRangeInput, dispatch }) { // eslint-disable-line no-inner-declarations
+	function AllComponents ({ // eslint-disable-line no-inner-declarations
+		textInput,
+		dateRangeInput,
+		dateRangeInput2,
+		dispatch,
+		dateRangeInputDispatch,
+		dateRangeInputDispatch2,
+	}) {
 		return <div>
 			<TextInput {...textInput} label="text input" onChange={v => dispatch(TextInput.actions.setValue(v))} />
 			<DateRangeInput
 				{...dateRangeInput}
-				onChange={d => dispatch(DateRangeInput.actions.setDateRangeValue(d))}
+				onChange={d => dateRangeInputDispatch(DateRangeInput.actions.setDateRangeValue(d))}
 				options={optionsDateRangeInput}
-				dispatch={dispatch}
+				dispatch={dateRangeInputDispatch}
+			/>
+			<DateRangeInput
+				{...dateRangeInput2}
+				onChange={d => dateRangeInputDispatch2(DateRangeInput.actions.setDateRangeValue(d))}
+				options={optionsDateRangeInput}
+				dispatch={dateRangeInputDispatch2}
 			/>
 		</div>
+	}
+
+	function render () { // eslint-disable-line no-inner-declarations
+		const props = { ...store.getState(), dispatch: store.dispatch, dateRangeInputDispatch, dateRangeInputDispatch2 }
+		const dom = AllComponents(props)
+		const node = document.getElementById('root')
+		Inferno.render(dom, node)
+	}
+
+	function createScopedReducer (scope, reducer) {  // eslint-disable-line no-inner-declarations
+		return (state, action) => state === undefined || action.scope == scope
+			? reducer(state, action)
+			: state
+	}
+
+	function createScopedDispatch (scope, dispatch) {  // eslint-disable-line no-inner-declarations
+		return action => typeof action == 'function'
+			? dispatch((thunkDispatch, ...thunkExtraArgs) => action(
+				createScopedDispatch(scope, thunkDispatch),
+				...thunkExtraArgs
+			))
+			: dispatch({ ...action, scope })
 	}
 }
